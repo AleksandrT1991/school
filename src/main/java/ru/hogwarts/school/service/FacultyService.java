10086c351abf2cb21d2ded48d62cab50ae0e7d01
AleksandrTsygulev <aleksandr.tsygulev@gmail.com>
2022-09-11 22:42:39 +0300

    ДЗ.

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,8 +1,13 @@
 package ru.hogwarts.school.service;
 
+
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.repositories.FacultyRepository;
+
+import java.util.Collection;
+import java.util.Optional;
+
 
 @Service
 public class FacultyService {
@@ -21,9 +26,24 @@ public class FacultyService {
         return facultyRepository.findById(id).orElse(null);
     }
     public Faculty editFaculty(Faculty faculty) {
-        return facultyRepository.save(faculty);
+        Optional <Faculty> optional = facultyRepository.findById(faculty.getId());
+        if (!optional.isPresent()) {
+            return null;
+        }
+        Faculty fromDb = optional.get();
+        fromDb.setName(faculty.getName());
+        fromDb.setColor(faculty.getColor());
+        return facultyRepository.save(fromDb);
     }
     public void deleteFaculty(long id) {
         facultyRepository.deleteById(id);
+    }
+
+    public Collection<Faculty> findByColor(String color) {
+        return facultyRepository.findFacultyByColor(color);
+    }
+
+    public Collection<Faculty> findByName(String name) {
+        return facultyRepository.findFacultyByName(name);
     }
 }
