d8da82d75e9929e56ef82fcd00832319f562f613
AleksandrTsygulev <aleksandr.tsygulev@gmail.com>
2022-09-29 22:09:43 +0300

    ДЗ 4.3

--- src/main/resources/liquibase/scripts/index-practice.sql
+++ src/main/resources/liquibase/scripts/index-practice.sql
@@ -3,6 +3,5 @@
 -- changeset a.tsygulev:1
 CREATE INDEX student_name_index ON student (name);
 -- changeset a.tsygulev:2
-CREATE INDEX faculty_name_index ON faculty (name);
--- changeset a.tsygulev:3
-CREATE INDEX faculty_color_index ON faculty (color);
+CREATE INDEX faculty_name_color_index ON faculty (name, color);
+
